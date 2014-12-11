283748307312f033e1973b7b0fdd94a9adc32b03
Kangmin Won <englekk@naver.com>
2014-12-11 13:07:36 +0900

    Refactoring

--- src/Corona/main.lua
+++ src/Corona/main.lua
@@ -17,6 +17,15 @@ require("CommonSettings")
 -- 완전한 풀스크린을 위한 코드
 -- native.setProperty( "androidSystemUiVisibility", "immersive" )
 
--- 시작점
-local composer = require "composer"
-composer.gotoScene("MainSceneStarter")
+local function on_SystemEvent(e)
+	local _type = e.type
+	if _type == "applicationStart" then -- 앱이 시작될 때
+		-- 시작점
+		local composer = require "composer"
+		composer.gotoScene("MainSceneStarter")
+--	elseif _type == "applicationExit" then -- 앱이 완전히 종료될 때
+--	elseif _type == "applicationSuspend" then -- 전화를 받거나 홈 버튼 등을 눌러서 앱을 빠져나갈 때
+--	elseif _type == "applicationResume" then -- Suspend 후 다시 돌아왔을 때
+	end
+end
+Runtime:addEventListener("system", on_SystemEvent)
