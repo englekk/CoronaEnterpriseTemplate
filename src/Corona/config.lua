7e25bd39a292cf1903f25fc13e6f170561b5320b
Kangmin Won <englekk@naver.com>
2015-01-15 17:28:31 +0900

    Licence

--- src/Corona/config.lua
+++ src/Corona/config.lua
@@ -1,7 +1,11 @@
 ----------------------------------------
--- 코로나 SDK 유료 계정 35% 할인 문의: englekk@naver.com
+-- 이 주석은 삭제하지 마세요.
+-- 35% 할인해 드립니다. 코로나 계정 유료 구매시 연락주세요. (Corona SDK, Enterprise, Cards)
 -- @Author 아폴로케이션 원강민 대표
--- @Website http://apollocation.com, http://wonhada.com
+-- @Website http://WonHaDa.com, http://Apollocation.com, http://CoronaLabs.kr
+-- @E-mail englekk@naver.com, englekk@apollocation.com
+-- 'John 3:16, Psalm 23'
+-- MIT License :: WonHada Library에 한정되며, 라이선스와 저작권 관련 명시만 지켜주면 되는 라이선스
 ----------------------------------------
 
 if system.getInfo("model") == nil then return end -- for HTML5
